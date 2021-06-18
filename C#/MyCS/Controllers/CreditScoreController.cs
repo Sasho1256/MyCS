4a8acd8bd1af5517dbefa65a7e4b6d4f0e75fcba
elichka <ed44362072@edu.mon.bg>
2021-06-18 14:23:09 +0300

    Add views for Calculator Score controller

--- C#/MyCS/Controllers/CreditScoreController.cs
+++ C#/MyCS/Controllers/CreditScoreController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InputModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MyCS.Controllers
 {
@@ -10,6 +11,23 @@ namespace MyCS.Controllers
         }
 
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Manual()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Manual(ManualInputModel input)
+        {
+            return View();
+        }
+
+        public IActionResult Bulk()
         {
             return View();
         }
